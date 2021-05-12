73745f957f334ecaed5819934b1ea0febaeba26e
Valerii Kvasko <valeriyimposibru@gmail.com>
2021-05-12 15:39:33 +0000

    code 1.0

---
+++ code.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+cout << "Hello World\n";
+cout << "Input: ";
+string data;
+cin >> data;
+cout << "Output: " << data << "\n\n";
+return 0;
+}
